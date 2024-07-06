924b86c4e2409a7fba996c1ce5b8dbfdb070f8e0
Ismail <ia.abousalem@gmail.com>
2024-07-06 10:30:52 -0400

    first commit and creating repo

---
+++ src/main/java/com/programme/ProgramMe/ProgramMeApplication.java
@@ -0,0 +1,13 @@
+package com.programme.ProgramMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programme/ProgramMe/ProgramMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programme.ProgramMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
