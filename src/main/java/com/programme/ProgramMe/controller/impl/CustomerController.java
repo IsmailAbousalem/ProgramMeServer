968b1b6811a5654c1d5dabaa39ba56451df60db0
Ismail <ia.abousalem@gmail.com>
2024-07-10 13:32:02 -0400

    added all endpoints for Customer, CRUD

--- src/main/java/com/programme/ProgramMe/controller/impl/CustomerController.java
+++ src/main/java/com/programme/ProgramMe/controller/impl/CustomerController.java
@@ -1,15 +1,42 @@
 package com.programme.ProgramMe.controller.impl;
 
+import com.programme.ProgramMe.model.Customer;
 import com.programme.ProgramMe.service.impl.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
+
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping("/{id}")
+    public Customer getCustomerById(@PathVariable Long id) {
+        return customerService.getCustomerById(id);
+    }
+
+    @PostMapping
+    public Customer createCustomer(@RequestBody Customer customer) {
+        return customerService.createCustomer(customer);
+    }
+
+    @PutMapping("/{id}")
+    public Customer updateCustomer(@PathVariable Long id, @RequestBody Customer customerDetails) {
+        return customerService.updateCustomer(id, customerDetails);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable Long id) {
+        customerService.deleteCustomer(id);
+    }
 
     // GET, POST, PUT, DELETE methods
 }
--- src/main/java/com/programme/ProgramMe/service/impl/CustomerService.java
+++ src/main/java/com/programme/ProgramMe/service/impl/CustomerService.java
@@ -1,13 +1,44 @@
 package com.programme.ProgramMe.service.impl;
 
+import com.programme.ProgramMe.model.Customer;
 import com.programme.ProgramMe.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public Customer getCustomerById(Long id) {
+        return customerRepository.findById(id).orElseThrow(() -> new RuntimeException("Customer not found"));
+
+//                new ResourceNotFoundException("Customer not found"));
+    }
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Customer updateCustomer(Long id, Customer customerDetails) {
+        Customer customer = getCustomerById(id);
+        customer.setEmail(customerDetails.getEmail());
+        customer.setName(customerDetails.getName());
+        customer.setNumber(customerDetails.getNumber());
+        customer.setPassword(customerDetails.getPassword());
+        return customerRepository.save(customer);
+    }
+
+    public void deleteCustomer(Long id) {
+        Customer customer = getCustomerById(id);
+        customerRepository.delete(customer);
+    }
 
     // CRUD methods
 }
