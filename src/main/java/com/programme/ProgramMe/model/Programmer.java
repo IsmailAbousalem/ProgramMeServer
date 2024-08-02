b820fe49cbb9146758b51dbfb98ca2e6af6f84a2
Ismail <ia.abousalem@gmail.com>
2024-08-02 00:15:52 -0400

    Programmer JWT Authentication Working

--- src/main/java/com/programme/ProgramMe/model/Programmer.java
+++ src/main/java/com/programme/ProgramMe/model/Programmer.java
@@ -1,31 +1,16 @@
 package com.programme.ProgramMe.model;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Programmer extends Customer {
     private String skills;
     private String description;
-
-
-    public Programmer() {
-    }
-
-    public String getSkills() {
-        return skills;
-    }
-
-    public void setSkills(String skills) {
-        this.skills = skills;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
 
 }
