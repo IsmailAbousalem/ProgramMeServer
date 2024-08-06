c1a8a613cc1a0cfb1b22526365303aa88970c309
Ismail <ia.abousalem@gmail.com>
2024-08-05 20:14:29 -0400

    Deleted all Image related controllers and classes

--- src/main/java/com/programme/ProgramMe/repository/PostImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.programme.ProgramMe.repository;
-
-import com.programme.ProgramMe.model.PostImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostImageRepository extends JpaRepository<PostImage, Long> {
-
-}
